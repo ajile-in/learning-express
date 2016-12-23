e93b12582203116799de5c23bbc98cfb16e7fcdb
Ajay Jadhav <ajay@ayanworks.com>
2016-12-23 21:13:05 +0530

    learning express

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+var nodemon = require('gulp-nodemon');
+
+var jsFiles = ['*.js', 'src/**/*.js'];
+
+gulp.task('style', function () {
+    gulp.src(jsFiles)
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish', {
+            verbose: true
+        }))
+        .pipe(jscs());
+});
+
+// Injecting & wire-dep is not done here
+//gulp.task('inject', function () {
+//    
+//});
+
+gulp.task('serve', ['style'], function () {
+    var options = {
+        script: 'app.js',
+        delayTime: 1,
+        env: {
+            'PORT': 3000
+        },
+        watch: jsFiles
+    };
+
+    return nodemon(options)
+        .on('restart', function (ev) {
+            console.log('Restarting...');
+        });
+});
